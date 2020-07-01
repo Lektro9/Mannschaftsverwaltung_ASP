64d35a6e35871c512d4ad2c50f0e5114e0b2a0d9
Lektro9 <larskr93@gmail.com>
2020-07-01 09:54:39 +0200

    change parse datetime so azure will host everything, also change net framework to 4.7.2

--- Mannschaftsverwaltung/Views/Personenverwaltung.aspx.cs
+++ Mannschaftsverwaltung/Views/Personenverwaltung.aspx.cs
@@ -14,6 +14,7 @@ using System.Web.Script.Serialization;
 using System.IO;
 using Newtonsoft.Json;
 using System.Xml.Serialization;
+using System.Globalization;
 
 namespace Mannschaftsverwaltung
 {
@@ -46,7 +47,7 @@ namespace Mannschaftsverwaltung
 
         protected void Page_Init(object sender, EventArgs e)
         {
-            
+
         }
 
         protected void Page_Load(object sender, EventArgs e)
@@ -59,26 +60,26 @@ namespace Mannschaftsverwaltung
             {
                 this.Session["Verwalter"] = new Controller();
                 Verwalter = (Controller)this.Session["Verwalter"];
-                FussballSpieler p1 = new FussballSpieler(1, "Shidoski", "Klaus", DateTime.Parse("01-01-1993"), "Stürmer", 23, anzahlJahre: 1, anzahlSpiele: 32, anzahlVereine: 2, gewonneneSpiele: 2);
-                FussballSpieler p2 = new FussballSpieler(2, "Johnsons", "Dennis", DateTime.Parse("02-01-1993"), "Stürmer", 25, anzahlJahre: 6, anzahlSpiele: 567, anzahlVereine: 2, gewonneneSpiele: 234);
-                FussballSpieler p4 = new FussballSpieler(3, "Redgrave", "Vergil", DateTime.Parse("03-01-1993"), "Stürmer", 857, anzahlJahre: 2, anzahlSpiele: 234, anzahlVereine: 5, gewonneneSpiele: 65);
-                FussballSpieler p5 = new FussballSpieler(4, "Redgrave", "Dante", DateTime.Parse("04-01-1993"), "Stürmer", 900, anzahlJahre: 8, anzahlSpiele: 199, anzahlVereine: 6, gewonneneSpiele: 4);
-                FussballSpieler p6 = new FussballSpieler(5, "Son", "Goku", DateTime.Parse("05-02-1993"), "Stürmer", 1010, anzahlJahre: 9, anzahlSpiele: 23, anzahlVereine: 1, gewonneneSpiele: 16);
-                Trainer t1 = new Trainer(6, "Taylor", "Tom", DateTime.Parse("01-01-1993"), 23);
+                FussballSpieler p1 = new FussballSpieler(1, "Shidoski", "Klaus", DateTime.ParseExact("02-01-1993", "dd-MM-yyyy", CultureInfo.InvariantCulture), "Stürmer", 23, anzahlJahre: 1, anzahlSpiele: 32, anzahlVereine: 2, gewonneneSpiele: 2);
+                FussballSpieler p2 = new FussballSpieler(2, "Johnsons", "Dennis", DateTime.ParseExact("02-01-1993", "dd-MM-yyyy", CultureInfo.InvariantCulture), "Stürmer", 25, anzahlJahre: 6, anzahlSpiele: 567, anzahlVereine: 2, gewonneneSpiele: 234);
+                FussballSpieler p4 = new FussballSpieler(3, "Redgrave", "Vergil", DateTime.ParseExact("03-01-1993", "dd-MM-yyyy", CultureInfo.InvariantCulture), "Stürmer", 857, anzahlJahre: 2, anzahlSpiele: 234, anzahlVereine: 5, gewonneneSpiele: 65);
+                FussballSpieler p5 = new FussballSpieler(4, "Redgrave", "Dante", DateTime.ParseExact("04-01-1993", "dd-MM-yyyy", CultureInfo.InvariantCulture), "Stürmer", 900, anzahlJahre: 8, anzahlSpiele: 199, anzahlVereine: 6, gewonneneSpiele: 4);
+                FussballSpieler p6 = new FussballSpieler(5, "Son", "Goku", DateTime.ParseExact("05-02-1993", "dd-MM-yyyy", CultureInfo.InvariantCulture), "Stürmer", 1010, anzahlJahre: 9, anzahlSpiele: 23, anzahlVereine: 1, gewonneneSpiele: 16);
+                Trainer t1 = new Trainer(6, "Taylor", "Tom", DateTime.ParseExact("01-01-1993", "dd-MM-yyyy", CultureInfo.InvariantCulture), 23);
 
-                HandballSpieler h1 = new HandballSpieler(7, "Ball", "Bernd", DateTime.Parse("01-03-1995"), "Verteidiger", 3, anzahlJahre: 2, anzahlSpiele: 77, anzahlVereine: 8, gewonneneSpiele: 56);
-                HandballSpieler h3 = new HandballSpieler(8, "Potter", "Harry", DateTime.Parse("16-07-1999"), "Stürmer", 25, anzahlJahre: 4, anzahlSpiele: 182, anzahlVereine: 1, gewonneneSpiele: 245);
-                HandballSpieler h2 = new HandballSpieler(9, "Dohnson", "Henry", DateTime.Parse("12-06-1963"), "Stürmer", 16, anzahlJahre: 6, anzahlSpiele: 90, anzahlVereine: 2, gewonneneSpiele: 78);
-                HandballSpieler h4 = new HandballSpieler(10, "Hamper", "Holly", DateTime.Parse("05-01-1995"), "Mittelfeld", 17, anzahlJahre: 10, anzahlSpiele: 33, anzahlVereine: 3, gewonneneSpiele: 98);
+                HandballSpieler h1 = new HandballSpieler(7, "Ball", "Bernd", DateTime.ParseExact("01-03-1995", "dd-MM-yyyy", CultureInfo.InvariantCulture), "Verteidiger", 3, anzahlJahre: 2, anzahlSpiele: 77, anzahlVereine: 8, gewonneneSpiele: 56);
+                HandballSpieler h3 = new HandballSpieler(8, "Potter", "Harry", DateTime.ParseExact("16-07-1999", "dd-MM-yyyy", CultureInfo.InvariantCulture), "Stürmer", 25, anzahlJahre: 4, anzahlSpiele: 182, anzahlVereine: 1, gewonneneSpiele: 245);
+                HandballSpieler h2 = new HandballSpieler(9, "Dohnson", "Henry", DateTime.ParseExact("12-06-1963", "dd-MM-yyyy", CultureInfo.InvariantCulture), "Stürmer", 16, anzahlJahre: 6, anzahlSpiele: 90, anzahlVereine: 2, gewonneneSpiele: 78);
+                HandballSpieler h4 = new HandballSpieler(10, "Hamper", "Holly", DateTime.ParseExact("05-01-1995", "dd-MM-yyyy", CultureInfo.InvariantCulture), "Mittelfeld", 17, anzahlJahre: 10, anzahlSpiele: 33, anzahlVereine: 3, gewonneneSpiele: 98);
 
-                TennisSpieler ts1 = new TennisSpieler(11, "Federer", "Roger", DateTime.Parse("01-12-2001"), 95, anzahlJahre: 12, anzahlSpiele: 2, anzahlVereine: 1, gewonneneSpiele: 23);
+                TennisSpieler ts1 = new TennisSpieler(11, "Federer", "Roger", DateTime.ParseExact("01-12-2001", "dd-MM-yyyy", CultureInfo.InvariantCulture), 95, anzahlJahre: 12, anzahlSpiele: 2, anzahlVereine: 1, gewonneneSpiele: 23);
 
-                Physiotherapeut ph1 = new Physiotherapeut(12, "Denrasen", "Dr. med", DateTime.Parse("01-01-1993"), "Auszeichnung blabla");
+                Physiotherapeut ph1 = new Physiotherapeut(12, "Denrasen", "Dr. med", DateTime.ParseExact("01-01-1993", "dd-MM-yyyy", CultureInfo.InvariantCulture), "Auszeichnung blabla");
                 Verwalter.Personen = new List<Person>() { p1, p2, p4, p5, p6, t1, h1, h2, h3, h4, ts1, ph1 };
             }
             LoadAllEditInputFields();
             LoadPersonen();
-            
+
         }
 
         protected void Button2_Click(object sender, EventArgs e)
@@ -578,7 +579,7 @@ namespace Mannschaftsverwaltung
         }
         public void disableAllRadioButtons()
         {
-            foreach(ListItem radioButton in RadioButtonList1.Items)
+            foreach (ListItem radioButton in RadioButtonList1.Items)
             {
                 radioButton.Enabled = false;
             }
